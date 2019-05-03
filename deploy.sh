6a230ee9495545fc140d4853c3788035690f7f3b
Adrian Holovaty <adrian@holovaty.com>
2019-05-03 10:04:26 +0200

    Update deploy.sh to bring it in line with upstream changes.
    * Added a comment pointing to the source documentation
      for this file.
    
    * Moved compilation logic into a doCompile function,
      as recommended by the upstream docs (and helps
      keep things organized).
    
    * Un-hardcoded Joe's email address, to remove redundancy
      between this file and .travis.yml.
    
    * Tweaked commit message to explicitly say 'automated update',
      so it's easier for people to realize these are automated
      commits.
    
    * Changed 'git add' to add all files, not just a hard-coded
      list. This may result in a bunch of new files being committed
      to the gh-pages branch the next time a Travis job runs;
      that's perfectly OK.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+
+# This script is heavily based on this document:
+# https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+
 set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
+
+function doCompile {
+    # At this point, bikeshed has already been run (see .travis.yml).
+    # Here, we merely do some cleanup.
+
+    # Clean out existing contents
+    rm -rf out/specification/index.html
+
+    # Copy files
+    cp -fv *.md ./out
+    cp -fv examples/* ./out/examples
+    cp -fv specification/index.html ./out/specification
+    cp -fv specification/images/* ./out/specification/images
+}
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
@@ -16,25 +34,19 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-# Clean out existing contents
-rm -rf out/specification/index.html
-
-# Copy files
-cp -fv *.md ./out
-cp -fv examples/* ./out/examples
-cp -fv specification/index.html ./out/specification
-cp -fv specification/images/* ./out/specification/images
+# Run our compile script
+doCompile
 
 # Now let's go have some fun with the cloned repo
 cd out
 git config user.name "Travis CI"
-git config user.email "joe@noteflight.com"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
@@ -44,9 +56,8 @@ fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add -f specification/index.html
-git add -f *.md
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+git add -A .
+git commit -m "Automated update of GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
