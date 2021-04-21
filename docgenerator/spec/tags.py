d80ca1b177f14d0fed263c06eaf875cde858c974
Adrian Holovaty <adrian@holovaty.com>
2021-04-21 13:39:04 +0200

    docgenerator: Changed sidebar HTML to use relative URLs.
    Since we allow arbitrary HTML in the SiteOptions.sidebar_html
    field, this HTML might include links -- in which case those
    links would be broken upon generating static docs. This commit
    fixes that by running a quick regex on the links, calling
    get_relative_url() on any href='' it found within.

--- docgenerator/spec/tags.py
+++ docgenerator/spec/tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.urls import reverse
+from django.utils.safestring import mark_safe
 from spec.utils.relative_url import get_relative_url
+import re
 
 register = template.Library()
 
@@ -12,3 +14,15 @@ register.simple_tag(takes_context=True)(relative_url)
 def relative_url_string(context, url_string):
     return get_relative_url(context['request'].path, url_string)
 register.simple_tag(takes_context=True)(relative_url_string)
+
+def make_urls_relative_callback(path, match):
+    return get_relative_url(path, match.group(0))
+
+def make_urls_relative(context, html):
+    html = re.sub(
+        r'(?<=href=")[^"]+',
+        lambda match: make_urls_relative_callback(context['request'].path, match),
+        html
+    )
+    return mark_safe(html)
+register.simple_tag(takes_context=True)(make_urls_relative)
