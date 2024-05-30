4a2dca567fa89cad0db151f05f2f4114d7e1d966
Adrian Holovaty <adrian@holovaty.com>
2024-05-30 14:02:18 +0200

    Changed docs system to serialize newline strings as lists of strings.
    Previously, when serializing strings into our doc system's
    JSON format, newlines were represented as the literal '\n'
    character, resulting in very long lines that were hard to
    read.
    
    Now, we detect any strings with newlines in them, and we
    convert these to lists of strings before serializing.
    This means the JSON serialization is a bit easier to read.
    
    This JSON file is purely an internal thing, so it's not
    really intended to be human-readable, but this was a low-effort
    way to make it a bit nicer.
    
    Note that this is backwards compatible, so any JSON file
    that was serialized in the old way will still be openable. It'll
    be converted to the new format the next time it's serialized
    (via 'manage.py freezedb').

--- docgenerator/spec/utils/serialization.py
+++ docgenerator/spec/utils/serialization.py
@@ -1,6 +1,7 @@
 from spec.models import *
 from django.contrib.auth.models import User
 from django.core import serializers
+from django.db import models
 import datetime
 import os
 
@@ -30,8 +31,43 @@ MODELS_TO_FREEZE = (
     StaticPage
 )
 
+class MonkeypatchedTextField(models.TextField):
+    """
+    This monkeypatches TextField so that the Django serialization
+    framework will serialize strings differently:
+
+    * If a string contains a newline character, the string will
+    be serialized as a list of strings (split on newlines).
+
+    * If a string doesn't contain a newline character, it will be
+    serialized as normal (just as a normal string).
+    """
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        if '\n' in value:
+            return value.split('\n')
+        return str(value)
+
+    def to_python(self, value):
+        if isinstance(value, list):
+            return '\n'.join(value)
+        return super().to_python(value)
+
+def monkeypatch_model(ModelClass):
+    """
+    Monkeypatches the given Django model class to replace any TextField
+    with our custom MonkeypatchedTextField.
+
+    Run this on every model class before serializing or deserializing,
+    in order to serialize every text-with-newlines string into a list.
+    """
+    for field in ModelClass._meta.fields:
+        if field.__class__ == models.TextField:
+            field.__class__ = MonkeypatchedTextField
+
 def all_model_objects():
     for ModelClass in MODELS_TO_FREEZE:
+        monkeypatch_model(ModelClass)
         for obj in ModelClass.objects.all():
             # Avoid spamming the diffs with people's last_login
             # or date_joined, which will differ across machines.
@@ -67,6 +103,7 @@ def thaw(infile:str):
     # Clear existing data.
     for ModelClass in reversed(MODELS_TO_FREEZE):
         ModelClass.objects.all().delete()
+        monkeypatch_model(ModelClass)
 
     # Import fresh data.
     could_not_import = []
